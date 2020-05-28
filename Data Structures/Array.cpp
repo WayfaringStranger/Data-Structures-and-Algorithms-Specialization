6277847fa76167340860130f9d9c65cab2a87eb9
WayfaringStranger <shubhampaitrik8@gmail.com>
2020-05-28 08:53:27 +0530

    Arrays

---
+++ Data Structures/Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int marks[5] = {27, 28, 29, 25, 24};
+
+	for (int i = 0; i < 5; i++) {
+		cout << marks[i] << endl;
+	}
+}
---
+++ Data Structures/Array.java
@@ -0,0 +1,10 @@
+class Array {
+	public static void main(String[] args) {
+		
+		String languages[] = {"Java", "Python", "C"};
+
+		for (int i = 0; i < languages.length; i++) {
+			System.out.println(languages[i]);
+		}
+	}
+}
